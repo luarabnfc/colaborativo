d58c3e1137cdd633dc9c756472a3881adb70f0fc
luiz felipe <luh.ffelipe@hotmail.com>
2018-10-17 17:49:15 -0300

    Classe Dividir

---
+++ calculadoraoo/calculadoraoo/Dividir.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadoraoo
+{
+    class Dividir
+    {
+       public Dividir()
+        {
+            Console.WriteLine("Objeto Dividir Criado");
+        }
+        public Dividir (int num1, int num2)
+       {
+           int total = num1 / num2;
+           Console.WriteLine("Resultado da divisão: " + total);
+       }
+
+        public void DiviEterno()
+        {
+
+        }
+    }
+}
