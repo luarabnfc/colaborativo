2161d4e61583d9634c6f0b75bb83c24e8090d08a
luiz felipe <luh.ffelipe@hotmail.com>
2018-10-17 18:48:10 -0300

    Atualização

--- calculadoraoo/calculadoraoo/Dividir.cs
+++ calculadoraoo/calculadoraoo/Dividir.cs
@@ -8,15 +8,20 @@ namespace calculadoraoo
 {
     class Dividir
     {
-       public Dividir()
+
+        public int num1, n2, total;
+
+        public Dividir()
         {
             Console.WriteLine("Objeto Dividir Criado");
         }
-        public Dividir (int num1, int num2)
-       {
-           int total = num1 / num2;
-           Console.WriteLine("Resultado da divisão: " + total);
-       }
+        public Dividir(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.n2 = num2;
+            int total = num1 / num2;
+            Console.WriteLine("Resultado da divisão: " + this.total);
+        }
 
         public void DiviEterno()
         {
--- calculadoraoo/calculadoraoo/Program.cs
+++ calculadoraoo/calculadoraoo/Program.cs
@@ -10,6 +10,20 @@ namespace calculadoraoo
     {
         static void Main(string[] args)
         {
+            //cria variavel da classe Div.
+            Dividir div;
+
+            //O operador "new" cria um objeto da classe;
+            div = new Dividir( );
+
+            div.num1 = 5;
+            div.n2 = 15;
+            div.total = div.num1 / div.n2;
+
+            Console.WriteLine("Primeiro numero: " + div.num1);
+            Console.WriteLine("Segundo numero: " + div.n2);
+
+            Console.ReadKey();
         }
     }
 }
